5b71882eec87c9d1d4f34935626cbf2da06bfc73
Rami Alrwais <raalrwai@ucsc.edu>
2025-12-12 12:16:40 -0500

    Create sub agent for Manager Agent

---
+++ my_agent/agent.py
@@ -0,0 +1,21 @@
+from google.adk.agents.llm_agent import Agent
+from .agents.stock_agent import stock_agent
+
+ROOT_MANAGER_INSTR = """
+You are a helpful stock price coordinator.
+
+When the user asks about any stock price or ticker information,
+delegate the task to the stock_agent.
+
+Only respond directly if the question is not about stocks.
+"""
+
+manager_agent = Agent(
+    name="manager_agent",
+    description="Delegates stock price queries to stock_agent.",
+    model="gemma-3-27b-it",
+    instruction=ROOT_MANAGER_INSTR,
+    sub_agents=[stock_agent],  
+)
+
+root_agent = manager_agent
